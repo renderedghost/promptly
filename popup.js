71968722d28117831e6a931038d87375e37477fb
Barry Prendergast <barry.m.prendergast@gmail.com>
2023-05-29 06:33:32 +0200

    saving popup design - layout complete but needs branding redesign

--- popup.js
+++ popup.js
@@ -1,35 +1,60 @@
 window.onload = () => {
-    // Load prompts from storage
+  const searchBox = document.getElementById("search-box");
+
+  // Load prompts from storage
+  const loadPrompts = () => {
     chrome.storage.local.get("prompts", ({ prompts = [] }) => {
-        const promptList = document.getElementById("prompt-list");
+      const promptList = document.getElementById("prompt-list");
 
-        // Clear list
-        promptList.innerHTML = "";
+      // Clear list
+      promptList.innerHTML = "";
 
-        prompts.forEach((prompt) => {
-            const promptElement = createPromptElement(prompt);
-            promptList.appendChild(promptElement);
-        });
+      // Filter prompts based on search query
+      const searchQuery = searchBox.value.toLowerCase();
+      const filteredPrompts = prompts.filter((prompt) =>
+      prompt.title.toLowerCase().includes(searchQuery)
+      );
+
+      filteredPrompts.forEach((prompt) => {
+        const promptElement = createPromptElement(prompt);
+        promptList.appendChild(promptElement);
+      });
     });
+  };
 
-    // Add a click event to the create button
-    document.getElementById("create-button").addEventListener("click", () => {
-        chrome.tabs.create({ url: "createPrompt.html" });
-    });
+  // Initial load of prompts
+  loadPrompts();
+
+  // Add a click event to the create button
+  document.getElementById("create-button").addEventListener("click", () => {
+    chrome.tabs.create({ url: "createPrompt.html" });
+  });
+
+  // Add an input event to the search box to reload prompts when the search query changes
+  searchBox.addEventListener("input", loadPrompts);
 };
 
 // Helper function to create a prompt element
 function createPromptElement(prompt) {
   const li = document.createElement("li");
+  li.className = "prompt-item";
+
+  const promptGroup = document.createElement("div");
+  promptGroup.className = "prompt-group";
 
   const title = document.createElement("p");
   title.textContent = prompt.title;
+  title.className = "prompt-title";
 
   const preview = document.createElement("p");
   preview.textContent = prompt.content;
-  preview.style.whiteSpace = "nowrap";
-  preview.style.overflow = "hidden";
-  preview.style.textOverflow = "ellipsis";
+  preview.className = "prompt-content";
+
+  promptGroup.appendChild(title);
+  promptGroup.appendChild(preview);
+
+  const buttonGroup = document.createElement("div");
+  buttonGroup.className = "button-group";
 
   const useButton = document.createElement("button");
   useButton.textContent = "Use";
@@ -38,28 +63,29 @@ function createPromptElement(prompt) {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0];
       if (activeTab.url.startsWith("https://chat.openai.com")) {
-        chrome.tabs.sendMessage(activeTab.id, {
-          action: "usePrompt",
-          text: prompt.content,
-        });
-      }
-    });
+      chrome.tabs.sendMessage(activeTab.id, {
+        action: "usePrompt",
+        text: prompt.content,
+      });
+    }
   });
+});
 
-  const editButton = document.createElement("button");
-  editButton.textContent = "Edit";
-  editButton.classList.add("small");
-  editButton.addEventListener("click", () => {
-    // Save the ID of the prompt being edited
-    chrome.storage.local.set({ editId: prompt.id }, () => {
-      chrome.tabs.create({ url: "editPrompt.html" });
-    });
+const editButton = document.createElement("button");
+editButton.textContent = "Edit";
+editButton.classList.add("small");
+editButton.addEventListener("click", () => {
+  // Save the ID of the prompt being edited
+  chrome.storage.local.set({ editId: prompt.id }, () => {
+    chrome.tabs.create({ url: "editPrompt.html" });
   });
+});
 
-  li.appendChild(title);
-  li.appendChild(preview);
-  li.appendChild(useButton);
-  li.appendChild(editButton);
+buttonGroup.appendChild(editButton);
+buttonGroup.appendChild(useButton);
 
-  return li;
+li.appendChild(promptGroup);
+li.appendChild(buttonGroup);
+
+return li;
 }
